d6f4b5f097d1ebe7c248e2f086be6f951e9c6741
Mike Hyler <mhyler@zoho.com>
2025-09-04 13:38:48 -0400

    test: end to end

---
+++ src/_tests_/EndToEnd.test.js
@@ -0,0 +1,38 @@
+/** @jest-environment node */
+import puppeteer from "puppeteer";
+
+describe("show/hide event details", () => {
+  let browser;
+  let page;
+  beforeAll(async () => {
+    browser = await puppeteer.launch({
+      headless: true,
+      slowMo: 0,
+      timeout: 0,
+    });
+    page = await browser.newPage();
+    await page.goto("http://localhost:5173/");
+    await page.waitForSelector(".event");
+  });
+
+  afterAll(() => {
+    browser.close();
+  });
+
+  test("An event element is collapsed by default", async () => {
+    const eventDetails = await page.$(".event .event_details");
+    expect(eventDetails).toBeNull();
+  });
+
+  test("User can expand an event to see details", async () => {
+    await page.click(".event .details__btn");
+    const eventDetails = await page.$(".event .event__details");
+    expect(eventDetails).toBeDefined();
+  });
+
+  test("User can collapse an event to hide details", async () => {
+    await page.click(".event .details__btn");
+    const eventDetails = await page.$(".event .event__details");
+    expect(eventDetails).toBeNull();
+  });
+});
