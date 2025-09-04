e3c6f6a5ced69874eefb616412decec5c460fce8
Mike Hyler <mhyler@zoho.com>
2025-09-04 14:46:48 -0400

    test: added Atatus

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import * as atatus from "atatus-spa";
 
-createRoot(document.getElementById('root')).render(
+atatus.config("181bdc13e24243e3b45460cc051f8d9b").install();
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
